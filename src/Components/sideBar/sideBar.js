5a2b0b914fb3d23bacdb61476ba0f53f250da94d
John <johnadepelumi@gmail.com>
2022-05-25 08:34:16 +0100

    minor adjustments to design and layout

--- src/Components/sideBar/sideBar.js
+++ src/Components/sideBar/sideBar.js
@@ -1,29 +1,62 @@
-import React from 'react'
-import './sideBar.css'
+import React from "react";
+import "./sideBar.css";
 import { BsHouse } from "react-icons/bs";
 import { BsStar } from "react-icons/bs";
 import { BsPerson } from "react-icons/bs";
 import { BsPower } from "react-icons/bs";
 import { BsPeople } from "react-icons/bs";
 import { BsBook } from "react-icons/bs";
-import { Link} from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div>
-
-        <ul>
-            <li><h1>BlueBerry CRM</h1></li>
-         <Link style ={{textDecoration: 'none'}}  to = '/dashboard'>   <li> <BsHouse className='icons'/> Home</li> </Link>
-         <Link style ={{textDecoration: 'none'}} to= '/team-members'>  <li> <BsPeople className='icons'/> Team Members</li> </Link>
-         <Link style ={{textDecoration: 'none'}} to= '/clients'>   <li> <BsPerson className='icons'/> Clients</li> </Link>
-         <Link style ={{textDecoration: 'none'}} to= '/products'>   <li> <BsStar className='icons'/> Products</li> </Link>
-           <li> <BsBook className='icons'/> Profile</li> 
-           <Link style ={{textDecoration: 'none'}} to= '/'> <li className = 'logOut'> <BsPower className='icons'/> Log Out</li> </Link>
-        </ul>
+    <div className="sidebar">
+      <ul>
+        <li className="sidebar__brand">
+          <h1>BlueBerry CRM</h1>
+        </li>
+        <Link style={{ textDecoration: "none" }} to="/dashboard">
+          {" "}
+          <li>
+            {" "}
+            <BsHouse className="icons" /> Home
+          </li>{" "}
+        </Link>
+        <Link style={{ textDecoration: "none" }} to="/team-members">
+          {" "}
+          <li>
+            {" "}
+            <BsPeople className="icons" /> Team Members
+          </li>{" "}
+        </Link>
+        <Link style={{ textDecoration: "none" }} to="/clients">
+          {" "}
+          <li>
+            {" "}
+            <BsPerson className="icons" /> Clients
+          </li>{" "}
+        </Link>
+        <Link style={{ textDecoration: "none" }} to="/products">
+          {" "}
+          <li>
+            {" "}
+            <BsStar className="icons" /> Products
+          </li>{" "}
+        </Link>
+        <li>
+          {" "}
+          <BsBook className="icons" /> Profile
+        </li>
+        <Link style={{ textDecoration: "none" }} to="/">
+          {" "}
+          <li className="logOut">
+            {" "}
+            <BsPower className="icons" /> Log Out
+          </li>{" "}
+        </Link>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- src/Pages/Log in/Login.js
+++ src/Pages/Log in/Login.js
@@ -1,54 +1,69 @@
-import React from 'react'
-import './Login.css'
-import { useState } from 'react'
-
+import React from "react";
+import "./Login.css";
+import { useState } from "react";
 
 const Login = () => {
-  const[form, setForm] = useState({
-    email: "" ,
-    password: ""
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
   });
 
   const [data, setData] = useState({});
 
-  const handleChange = (e)=>{
+  const handleChange = (e) => {
     setForm({
-      ...form ,
-      [e.target.name]: e.target.value
-    })
-  }
- 
-  const submitForm = (e)=>{
-  e.preventDefault();
- setData({
-   ...form
- })
- sessionStorage.setItem('userDetails' , JSON.stringify({...form}));
- console.log(data)
- window.location = "/dashboard" ;
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  }
-   
+  const submitForm = (e) => {
+    e.preventDefault();
+    setData({
+      ...form,
+    });
+    sessionStorage.setItem("userDetails", JSON.stringify({ ...form }));
+    console.log(data);
+    window.location = "/dashboard";
+  };
+
   return (
-        <div className='bdy'>
-         <div className="formBox">
-             
+    <div className="bdy">
+      <div className="formBox">
         <h1>Login Form</h1>
         <p>Welcome Back </p>
 
-        <form className="loginForm"  onSubmit={submitForm}>
-            <div id="errorMessage"></div>
-           <div><input type="email" name="email" id="emailBox" className="formField" placeholder="Email Address" onChange={handleChange}/></div> 
-           <hr/>
-           
-           <div> <input type="password" name="password" id="passwordBox" className="formField" placeholder="Password" onChange={handleChange}/></div>
-           <hr/>
-          <div className="submit"><input type="submit" name="submit" id="submitButton" /></div>  
-        </form>
-    </div>
-    </div>
-    
-  )
-}
+        <form className="loginForm" onSubmit={submitForm}>
+          <div id="errorMessage"></div>
+          <div>
+            <input
+              type="email"
+              name="email"
+              id="emailBox"
+              className="formField"
+              placeholder="Email Address"
+              onChange={handleChange}
+            />
+          </div>
 
-export default Login
+          <div>
+            {" "}
+            <input
+              type="password"
+              name="password"
+              id="passwordBox"
+              className="formField"
+              placeholder="Password"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="submit">
+            <input type="submit" name="submit" id="submitButton" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
